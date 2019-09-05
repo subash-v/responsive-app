311c9b21b40ba4b209cc88722c1747834d74fee7
Subash V <42377150+subash-v@users.noreply.github.com>
2019-09-05 21:26:53 +0530

    added not found page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import { Switch,Route } from "react-router-dom";
 import './App.css';
 import Home from './components/Home';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
@@ -10,8 +11,8 @@ function App() {
     <Route exact path="/" component={Home} />
     {/* <Route exact path="/test" component={Test} />
     <Route exact path="/test/:id" component={TestId} />
-    <Route exact path="/check" component={Check} />
-    <Route component={NoMatch} /> */}
+    <Route exact path="/check" component={Check} /> */}
+    <Route component={NoMatch} />
   </Switch>
   );
 }
