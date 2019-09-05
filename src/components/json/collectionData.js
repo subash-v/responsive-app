a2b843b510d7236303a17166c984a9300ca7881f
Subash V <42377150+subash-v@users.noreply.github.com>
2019-09-05 16:58:16 +0530

    Add files via upload

---
+++ src/components/json/collectionData.js
@@ -0,0 +1,29 @@
+const collection = [
+    {
+      header: "Trending This Week",
+      subtext: "Most popular restaurants in town this week",
+      image:
+        "https://b.zmtcdn.com/data/collections/b53772a204429cb9b42313d6dc22bf3c_1556018415.jpg?fit=around%7C300%3A250&crop=300%3A250%3B%2A%2C%2A(25 kB)"
+    },
+    {
+      header: "Bengaluru's Finest ",
+      subtext:
+        "The hunt for the highest-rated restaurants in your city ends here",
+      image:
+        "https://b.zmtcdn.com/data/collections/53be5a745e959a8f6c51c52dd3d9edd1_1536825795.jpg?fit=around%7C300%3A250&crop=300%3A250%3B%2A%2C%2A(20 kB)"
+    },
+    {
+      header: "Newly Opened",
+      subtext: "The best new places in town",
+      image:
+        "https://b.zmtcdn.com/data/collections/7bef8e84e64ccf5b728c77510b7d52ad_1535355701.jpg?fit=around%7C300%3A250&crop=300%3A250%3B%2A%2C%2A(14 kB)"
+    },
+    {
+      header: "Insta-Worthy",
+      subtext:
+        "So aesthetically pleasing even the most camera shy will have their phones out at these Insta-perfect havens",
+      image:
+        "https://b.zmtcdn.com/data/collections/3e934519be19cd80ddc41c13721a8e4b_1566461030.jpg?fit=around%7C300%3A250&crop=300%3A250%3B%2A%2C%2A(17 kB)"
+    }
+   ];
+   export default collection;
